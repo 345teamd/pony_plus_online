23e143d7dd46abd1d8aef30443a0de6568be4c65
Joshua Mayne <jmayne1@umbc.edu>
2012-11-06 15:49:12 -0500

    Revert "Revert "Revert "Made the application twitter boostrap ready"""
    This reverts commit 974bf9be4d7e8bbc68dababff215dcb0941913ad.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Store::Application.config.secret_token = 'f7060d2a2c08360239df13e14722670c5d2a426c83e11d856dba77c9e2e800df7cf95444a9efbb6abc27d6d72c3f3c18324d14685a56028c8ca9e5a6fd9a6b36'
