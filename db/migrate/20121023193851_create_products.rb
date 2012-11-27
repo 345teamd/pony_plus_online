272ab7b9c5025a81a18ca765b0484a0653dee60f
Joshua Mayne <jmayne1@umbc.edu>
2012-11-27 10:24:43 -0500

    Revert "Revert "Revert "Revert "Made the application twitter boostrap ready""""
    This reverts commit 6270aedccc90d49ae4cff664820c8d525b180e6a.

---
+++ db/migrate/20121023193851_create_products.rb
@@ -0,0 +1,20 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.integer :id
+      t.string :title
+      t.text :description
+      t.string :image_url
+      t.decimal :price, :precision => 8, :scale => 2
+      t.string :category
+      t.integer :amount_online
+      t.integer :amount_store
+
+      t.timestamps
+    end
+  end
+  
+  def self.down
+    drop_table :products
+  end
+end
