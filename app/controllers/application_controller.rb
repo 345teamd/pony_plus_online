1884fb50243231b361005b6a222e0e74b89b9078
Joshua Mayne <jmayne1@umbc.edu>
2012-12-04 15:24:04 -0500

    Added Orders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   private
-  
     def current_cart
       Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
@@ -10,4 +9,5 @@ class ApplicationController < ActionController::Base
       session[:cart_id] = cart.id
       cart
     end
+    
 end
